bb93ce28d97b2c0175eb10e3ddb6cc24c5596da6
GG <stas.eryomchenko@mail.ru>
2025-08-03 14:35:34 +0700

    First request

---
+++ WebTaskManager/Endpoints/MyTaskendpoint.cs
@@ -0,0 +1,19 @@
+﻿using WebTaskManager.Contracts;
+using WebTaskManager.Interface;
+
+namespace WebTaskManager.Endpoints
+{
+    public static class MyTaskendpoint
+    {
+        //create mytask 
+        public static IEndpointRouteBuilder MapMyTaskEndPoint(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/myTasks", async (CreateMyTaskRequest CreateMyTask, IMyTaskService myTaskService) =>
+            {
+                var result = await myTaskService.AddMyTaskAsync(CreateMyTask);
+                return Results.Created($"/mytask/{result.Id}", result);
+            });
+            return app;
+        }
+    }
+}
